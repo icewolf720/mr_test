a99cbd7951d19b088d2f71404b4011dee9e61b8b
frank <czw0720@gmail.com>
2021-05-23 15:50:14 +0800

    finish

---
+++ src/pages/shopping/types/index.ts
@@ -0,0 +1,19 @@
+export type Product = {
+    id: string;
+    name: string;
+    price: number;
+    description: string;
+    sizeOptions: string[];
+    image: string;
+  };
+  
+  export interface ShoppingState {
+    product?: Product;
+    loading: boolean;
+  }
+  
+  export type AddToCardPayload = {
+    product: Product;
+    size: string;
+  };
+  
